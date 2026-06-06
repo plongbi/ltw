2b0c4c70c480968781e4409a5a3ade7fdae6d9ef
plongbi <longltv2005@gmail.com>
2026-06-06 06:06:02 +0000

    Update frontend and backend

---
+++ fe/src/components/LoginRegister/LoginRegister.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../lib/fetchModelData";
+
+export default function LoginRegister({ setCurrentUser }) {
+  const [login_name, setLoginName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [registerData, setRegisterData] = useState({
+    login_name: "",
+    password: "",
+    confirmPassword: "",
+    first_name: "",
+    last_name: "",
+    location: "",
+    description: "",
+    occupation: "",
+  });
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/admin/login`,
+        {
+          login_name,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      setCurrentUser(res.data);
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+    } catch (err) {
+      alert(
+        "Login failed: " + (err.response?.data?.message || "Unknown error")
+      );
+    }
+  };
+
+  const handleRegister = () => {
+    if (registerData.password !== registerData.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    alert(
+      `Register success!
+  Login Name: ${registerData.login_name}
+  First Name: ${registerData.first_name}
+  Last Name: ${registerData.last_name}
+  Location: ${registerData.location}
+  Description: ${registerData.description}
+  Occupation: ${registerData.occupation}`
+    );
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-wrapper">
+        <h1 className="login-title">Photo Sharing App</h1>
+
+        <div className="form-section">
+          <h2>Login</h2>
+
+          <div className="form-row">
+            <input
+              placeholder="Login name"
+              value={login_name}
+              onChange={(e) => setLoginName(e.target.value)}
+            />
+
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+
+        <div className="form-section">
+          <h2>Register</h2>
+
+          <div className="form-row">
+            {Object.keys(registerData).map((key) => (
+              <input
+                key={key}
+                type={key.includes("password") ? "password" : "text"}
+                placeholder={key.replace("_", " ")}
+                value={registerData[key]}
+                onChange={(e) =>
+                  setRegisterData({
+                    ...registerData,
+                    [key]: e.target.value,
+                  })
+                }
+              />
+            ))}
+
+            <button onClick={handleRegister}>Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ fe/src/components/TopBar/index.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+
+import { useLocation } from "react-router-dom";
+import { BASE_URL } from "../../lib/fetchModelData";
+import axios from "axios";
+
+function TopBar({ currentUser, setCurrentUser }) {
+  const location = useLocation();
+
+  const path = location.pathname;
+
+  let contextText = "Welcome to Photo App";
+
+  if (path.startsWith("/users/")) {
+    contextText = "User Detail";
+  } else if (path.startsWith("/photos/")) {
+    contextText = "User Photos";
+  }
+
+  // Logout
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        `${BASE_URL}/admin/logout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+
+      setCurrentUser(null);
+      localStorage.removeItem("currentUser");
+    } catch (err) {
+      alert("Logout failed");
+    }
+  };
+
+  return (
+    <AppBar position="static" className="topbar-appBar">
+      <Toolbar
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "20px",
+        }}
+      >
+        {/* LEFT */}
+        <Typography variant="h6">Phạm Hải Long - B23DCVT258</Typography>
+
+        {/* RIGHT */}
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "15px",
+          }}
+        >
+          {/* USER */}
+          <Typography variant="h6">Hi {currentUser?.first_name} </Typography>
+
+          <Typography variant="h6">{contextText}</Typography>
+
+          {/* UPLOAD */}
+          <Button style={{ color: "white" }}>Add photo</Button>
+
+          {/* LOGOUT */}
+          <Button style={{ color: "red" }} onClick={handleLogout}>
+            Logout
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default TopBar;
